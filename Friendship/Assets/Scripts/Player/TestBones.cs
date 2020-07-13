d31c9e48f175edeb41f06777719f9a3a80d5650c
TigAurora <tigaurora@gmail.com>
2020-07-13 05:52:34 -0700

    Character creation button update
    Character creation button update

---
+++ Friendship/Assets/Scripts/Player/TestBones.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBones : MonoBehaviour
+{
+    private Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject temp = null;
+            Transform[] trans = GameObject.Find("blindplayer").GetComponentsInChildren<Transform>(true);
+            foreach (Transform t in trans)
+            {
+                if (t.gameObject.name == "Player P1")
+                {
+                    temp = t.gameObject;
+                }
+            }
+            GameObject temp1 = GameObject.Find("blind1");
+            temp.SetActive(true);
+            temp1.SetActive(false);
+        }
+    }
+}
